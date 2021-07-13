0ef7aa05dad1bcedb33951b71c0850e8b79014d3
JustDoom <61824552+JustDoom@users.noreply.github.com>
2021-07-13 18:13:55 +1000

    more code

--- src/main/java/org/anythingmc/updatechecker/Requests.java
+++ src/main/java/org/anythingmc/updatechecker/Requests.java
@@ -9,13 +9,21 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.anythingmc.updatechecker.UpdateChecker.config;
 
 public class Requests {
+
     public static final String SPIGOT_API_URL = "https://api.spiget.org/v2/resources/";
     public HttpClient httpClient;
 
@@ -30,16 +38,21 @@ public class Requests {
     }
 
     public Optional<JsonObject> getSpigotPluginInfo(String url) {
+        //System.out.println(url);
+        if(url.endsWith("/"))
+            url = url.substring(0, url.length() - 1);
         int index = url.lastIndexOf("/");
         if (index == -1) {
             System.out.println("An invalid resource has been passed");
             return Optional.empty();
         }
         String resourceCode = url.substring(index);
+        resourceCode = resourceCode.replaceAll("/", "");
 
         HttpGet get = new HttpGet(SPIGOT_API_URL + resourceCode);
         try {
             HttpResponse response = httpClient.execute(get);  // stops here
+            System.out.println(get);
             int statusCode = response.getStatusLine().getStatusCode();
             if (statusCode != 200) {  // Status code 200 --> OK
                 System.out.println(response.getStatusLine().getReasonPhrase());
@@ -49,9 +62,42 @@ public class Requests {
             return Optional.of(element.getAsJsonObject());
         } catch (IOException error) {
             error.printStackTrace();
+            System.out.println("ERROR WITH " + get);
             return Optional.empty();
         } finally {
             get.releaseConnection();
         }
+    }
+
+    public List<String> getMdFile(String url){
+        try {
+
+            URL newUrl = new URL(url);
+
+            // read text returned by server
+            BufferedReader in = new BufferedReader(new InputStreamReader(newUrl.openStream()));
+
+            List<String> urls = new ArrayList<>();
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                if(line.contains("spigotmc.org/wiki"))
+                    continue;
+                Pattern pattern = Pattern.compile("(?<=\\().+?(?=\\))");
+                Matcher matcher = pattern.matcher(line);
+                if(matcher.find())
+                    urls.add(matcher.group());
+            }
+            in.close();
+
+            return urls;
+        }
+        catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
+        }
+        catch (IOException e) {
+            System.out.println("I/O Error: " + e.getMessage());
+        }
+        return null;
     }
 }
--- src/main/java/org/anythingmc/updatechecker/UpdateChecker.java
+++ src/main/java/org/anythingmc/updatechecker/UpdateChecker.java
@@ -8,6 +8,8 @@ import com.google.gson.JsonObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
@@ -17,11 +19,10 @@ import java.util.TimerTask;
 public class UpdateChecker {
 
     public static Gson gson = new Gson();
-    public static Link[] links;
+    public static Link links;
     public static Config config;
     public static Requests request;
     public static WebhookClient webhookClient;
-    public static Database database = new Database();
 
     public static boolean doesFileExist(String filename) throws IOException {
         Path path = Path.of(filename);
@@ -37,8 +38,6 @@ public class UpdateChecker {
 
     public static void main(String[] args) throws IOException {
 
-        // Check if resources exist in working directory
-        // TODO: logging
         boolean configExists = doesFileExist("config.json");
         boolean linksExists = doesFileExist("links.json");
 
@@ -49,13 +48,12 @@ public class UpdateChecker {
 
         // Parse the links
         String data = Files.readString(Path.of("links.json"));
-        links = gson.fromJson(data, Link[].class);
+        links = gson.fromJson(data, Link.class);
 
         // Parse the config
         data = Files.readString(Path.of("config.json"));
         config = gson.fromJson(data, Config.class);
 
-        // Initialise request object
         request = new Requests();
 
         // check if webhook url exists in config
@@ -72,7 +70,6 @@ public class UpdateChecker {
             System.exit(1);
         }
 
-        // Timer task
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
@@ -101,39 +98,91 @@ public class UpdateChecker {
 
         embedBuilder.setTitle(new WebhookEmbed.EmbedTitle("Beginning checking for updates...", null))
                 .setColor(0xFFAF33);
-        webhookClient.send(embedBuilder.build());
+        //webhookClient.send(embedBuilder.build());
 
-        // loop through all urls within the spigot list
-        for (String url : links[0].urls) {
-            isOutdated = false;
-            Optional<JsonObject> optionalObject = request.getSpigotPluginInfo(url);
+        // loop through all urls
+        for (String url : links.urls) {
 
-            // checks to see if the request was successful by checking if the optional object is empty or not
-            if (optionalObject.isPresent()) {
-                JsonObject object = optionalObject.get();
-                // TODO: Version checking
+            for(String resourceUrl:request.getMdFile(url)){
+                System.out.println(resourceUrl);
+                isOutdated = false;
 
-                if (!isOutdated) {
-                    upToDate++;
-                } else {
-                    outOfDate++;
-                    String name = object.get("name").getAsString();
-                    embedBuilder.setTitle(new WebhookEmbed.EmbedTitle(name, url))
-                            .setColor(0x00FFB9)
-                            .setDescription(String.format("`%s` is out of date, click [here](%s)", name, url));
-                    webhookClient.send(embedBuilder.build());
+                Optional<JsonObject> optionalObject = Optional.empty();
+
+                LinkSite site = getSiteType(resourceUrl);
+
+                if(site == null)
+                    continue;
+
+                if(site == LinkSite.SPIGOT) {
+                    optionalObject = request.getSpigotPluginInfo(resourceUrl);
+                } else if(site == LinkSite.GITHUB){
+                    continue;
+                    //TODO check for updates on github repo
+                } else if(site == LinkSite.POLYMART){
+                    continue;
+                    //TODO find polymart API
+                } else if(site == LinkSite.MCMARKET){
+                    continue;
+                    //TODO find polymart API
+                } else if(site == LinkSite.DISCORD){
+                    continue;
+                } else if(site == LinkSite.OTHER){
+                    continue;
                 }
-            } else {  // invoked when an error has occurred in the request
-                embedBuilder.setTitle(new WebhookEmbed.EmbedTitle("Check failed", url))
-                        .setDescription(String.format("Could not check for updates for [this](%s) project, an error has occurred", url))
-                        .setColor(0xFF0000);
-                webhookClient.send(embedBuilder.build());
-                System.out.println("Request failed for the url: " + url);
+
+                // checks to see if the request was successful by checking if the optional object is empty or not
+                if (optionalObject.isPresent()) {
+                    JsonObject object = optionalObject.get();
+                    // TODO: Version checking
+
+                    if (!isOutdated) {
+                        upToDate++;
+                    } else {
+                        outOfDate++;
+                        String name = object.get("name").getAsString();
+                        embedBuilder.setTitle(new WebhookEmbed.EmbedTitle(name, url))
+                                .setColor(0x00FFB9)
+                                .setDescription(String.format("`%s` is out of date, click [here](%s)", name, url));
+                        webhookClient.send(embedBuilder.build());
+                    }
+                } else {  // invoked when an error has occurred in the request
+                    embedBuilder.setTitle(new WebhookEmbed.EmbedTitle("Check failed", url))
+                            .setDescription(String.format("Could not check for updates for [this](%s) project, an error has occurred", url))
+                            .setColor(0xFF0000);
+                    //webhookClient.send(embedBuilder.build());
+                    //System.out.println("Request failed for the url: " + url);
+                }
             }
         }
+
         embedBuilder.setTitle(new WebhookEmbed.EmbedTitle("Finished checking for updates...", null))
                 .setDescription(String.format("Up to date: `%d`\nOut of date: `%d`", upToDate, outOfDate))
                 .setColor(0x2DEE12);
-        webhookClient.send(embedBuilder.build());
+        //webhookClient.send(embedBuilder.build());
+    }
+
+    private static LinkSite getSiteType(String url){
+        if(url.contains("spigotmc.org"))
+            return LinkSite.SPIGOT;
+
+        if(url.contains("github.com"))
+            return LinkSite.GITHUB;
+
+        if(url.contains("mc-market.org"))
+            return LinkSite.MCMARKET;
+
+        if(url.contains("polymart.org"))
+            return LinkSite.POLYMART;
+
+        if(url.contains("discord"))
+            return LinkSite.DISCORD;
+
+        try {
+            new URL(url);
+            return LinkSite.OTHER;
+        } catch (MalformedURLException e) {
+            return null;
+        }
     }
 }
